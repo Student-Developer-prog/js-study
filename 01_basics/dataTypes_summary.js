7810f0ec75cfbd7d2147d179231dbfc07fe393e0
Student-Developer-prog <vishayabhyasclasses@gmail.com>
2026-05-15 18:18:27 +0000

    data type summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,24 @@
+ const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id);
+// console.log(anotherId);
+// console.log(id === anotherId);
+
+const cars = ["honda", "Maruti" ,"Mahindra"]
+
+let myObj ={
+    name:"Kumar",
+    age:26
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+console.log( typeof myFunction);
+console.log(typeof myObj);
+console.log(typeof id);
+
+
+
+
