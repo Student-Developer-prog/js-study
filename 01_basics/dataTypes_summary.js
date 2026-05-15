c0d35535556c7752aeaee114070be22cd687595c
Student-Developer-prog <vishayabhyasclasses@gmail.com>
2026-05-15 18:52:24 +0000

    further addition

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -4,21 +4,37 @@
 // console.log(anotherId);
 // console.log(id === anotherId);
 
-const cars = ["honda", "Maruti" ,"Mahindra"]
+// const cars = ["honda", "Maruti" ,"Mahindra"]
 
-let myObj ={
-    name:"Kumar",
-    age:26
-}
+// let myObj ={
+//     name:"Kumar",
+//     age:26
+// }
 
-const myFunction = function(){
-    console.log("Hello world");
+// const myFunction = function(){
+//     console.log("Hello world");
     
-}
-console.log( typeof myFunction);
-console.log(typeof myObj);
-console.log(typeof id);
+// }
+// console.log( typeof myFunction);
+// console.log(typeof myObj);
+// console.log(typeof id);
 
+// let cmName = "Samarat"
+// let anothername = cmName
+//  anothername = "PK"
+
+// console.log(cmName);
+// console.log(anothername);
+
+let userOne = {
+    email: "kk@gmail.com",
+    add:"Kolkatta"
+}
+
+let userTwo = userOne
+userTwo.email = "gne@"
+console.log(userOne.email);
+console.log(userTwo.email);
 
 
 
