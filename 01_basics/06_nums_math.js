8a59826b6114f08d8023604b774126e24a966008
Student-Developer-prog <vishayabhyasclasses@gmail.com>
2026-05-17 18:09:35 +0000

    num math prog

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,30 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4));
+
+const largeNumber = 1000000
+console.log(largeNumber.toLocaleString());
+console.log(largeNumber.toLocaleString('en-IN'));
+
+
+
+
+
+
+
+
+
+
+
+
+
