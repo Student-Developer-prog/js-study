23f0525f723fe668004b64d48d383157974f5e32
Student-Developer-prog <vishayabhyasclasses@gmail.com>
2026-05-18 04:46:48 +0000

    about date and time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,43 @@
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+// let createdDate = new Date(2026, 0, 23)
+// console.log(createdDate);
+// console.log(createdDate.toDateString());
+
+let myCreateDate = new Date("01-14-2026")
+// console.log(myCreateDate);
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
