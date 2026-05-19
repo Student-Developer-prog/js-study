532b6bad476bee92499c7e615935b49311da5e2d
Student-Developer-prog <vishayabhyasclasses@gmail.com>
2026-05-19 05:12:43 +0000

    array part 2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,33 @@
+// const heros = ["Kalam", "Bhagat", "Chandrashekhar"]
+// const scientist = ["Vikram", "Neil", "Newton"]
+// heros.push(scientist)
+// console.log(heros);
+// console.log(heros[3]);
+// console.log(heros[3][1]);
+// console.log(heros.concat(scientist));
+// const all_combine=[...heros, ...scientist]
+// console.log(all_combine);
+
+// const another_array = [1, 2, 3, [4, 5, 6],7, [6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// const real_another_array_second=another_array.flat(2)
+// console.log(real_another_array);
+// console.log(real_another_array_second);
+// console.log();
+
+// console.log(Array.isArray("Patna"));
+// console.log(Array.from("Patna"));
+// console.log(Array.from({name: "Patna"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
+
+
