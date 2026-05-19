0d74d4ae0205ee43b073a7a0d006b0af98053f86
Student-Developer-prog <vishayabhyasclasses@gmail.com>
2026-05-19 04:26:42 +0000

    Array Part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+const myArr=[0, 1, 2, 3, 4, 5]
+const myHeros = ["Shaktiman", "nagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[0]);
+
+// console.log(myArr[1])
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.unshift(11)
+// myArr.shift()
+
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.includes(1));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(5));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof(newArr));
+
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1,3)
+const myn2 = myArr.slice(1,4)
+
+
+console.log(myn1);
+console.log(myn2);
+
+console.log("B ",myArr);
+
+
+
+
+
+
+
