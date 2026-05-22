674bcd2005dc46d6aedc08439d38175a1b4628d7
Student-Developer-prog <vishayabhyasclasses@gmail.com>
2026-05-22 05:11:57 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,20 @@
+// function addTwonumbers(num1,num2){
+//     console.log(num1+num2);
+    
+// }
+// const result = addTwonumbers(3,4)
+
+function addTwonumbers(num1,num2){
+    // let result = num1 + num2
+    // console.log("before return.");
+    
+    return num1+num2
+    // console.log("after return");
+    
+    
+}
+const result = addTwonumbers(3,4)
+// addTwonumbers(3,"4")
+// addTwonumbers(3,"a")
+// addTwonumbers(3,null)
+// console.log("Result: ", result);
