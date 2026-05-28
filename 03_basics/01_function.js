0de3078318a0967708664d5305a29536dcefaff5
Student-Developer-prog <vishayabhyasclasses@gmail.com>
2026-05-28 06:43:38 +0000

    Function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -18,3 +18,51 @@ const result = addTwonumbers(3,4)
 // addTwonumbers(3,"a")
 // addTwonumbers(3,null)
 // console.log("Result: ", result);
+
+function loginUserMessage(username){
+    if(username === undefined){
+        // console.log("please enter a username")
+        return
+    }
+    return `${username} just logged in`
+
+}
+
+// console.log(loginUserMessage("kumar"))
+
+// console.log(loginUserMessage(" "))
+// console.log(loginUserMessage( ))
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500,2000));
+
+const user = {
+    username: "Kumar",
+    price: 199
+}
+function handleObject(anyobject){
+   console.log(`Usernmae is ${anyobject.username} and price is ${anyobject.price}`);
+
+   
+}
+// handleObject(user)
+handleObject({
+    username: "Ram",
+    price: 499
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+     return getArray[0]
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([50,100,150]));
+
+
+
+
+
+
