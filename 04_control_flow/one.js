b41f04e446e05d28ecb2f299627c6554409541a0
Student-Developer-prog <vishayabhyasclasses@gmail.com>
2026-05-29 05:15:17 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature === 41) {
+//     console.log("less than 50.");
+    
+// }
+// else {
+// console.log("temp is greater than 50");
+// }
+
+// console.log("Executed.");
+
+// const score = 200
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`User power : ${power}`);
+    
+// }
+
+// console.log(`User power : ${power}`);
+
+// const balance = 1000
+
+// if (balance > 500) console.log("tested"),console.log("test2");
+// if (balance <500) {
+//     console.log("less than 5000");
+    
+    
+// } else if (balance <800){
+//     console.log("less than 800");
+    
+// }else {
+//     console.log("no above condition is met.");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course.");
+    
+}
