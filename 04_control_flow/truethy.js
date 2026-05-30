29c97c0211d464e2cb3b9a5ea9194bfb4ee2ef9b
Student-Developer-prog <vishayabhyasclasses@gmail.com>
2026-05-30 10:48:52 +0000

    truethy statment

---
+++ 04_control_flow/truethy.js
@@ -0,0 +1,29 @@
+const userEmail = []
+
+// if (userEmail) {
+//     console.log("Got user Email.");
+
+    
+// }else {
+//     console.log("Don't have user email.");
+    
+// }
+// if (userEmail.length === 0) {
+//     console.log("Array is empty.");
+    
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty.");
+    
+// }
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = null ?? 15
+val1 = null ?? 13??25
+
+console.log(val1);
